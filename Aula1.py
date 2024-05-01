83d628ec4720240e69fb9c10126df33ba380944b
joao0918 <joaopedroaranda19@gmail.com>
2024-05-01 14:44:45 -0400

    Python Alura

---
+++ Aula1.py
@@ -0,0 +1,33 @@
+"""
+ALURA   
+
+Aqui começa nossa jornada na alura, nessa 1 fase.
+Vamos fazer um prjeto de um aplicativo de entrega de comida, e vamos implementar uma dessas partes.
+"Sabor Express"
+
+Forma de manter os dados da aplicação.
+Vamos fazer a parte do back-And
+
+# Boas pratica de variaveis link: https://encurtador.com.br/BJMUW
+e também questao de aspas sempre utilizar a que foi iniciada ou melhor usar sempre a mesma.
+
+"""
+
+print("""
+░██████╗░█████╗░██████╗░░█████╗░██████╗░  ███████╗██╗░░██╗██████╗░██████╗░███████╗░██████╗░██████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗██╔══██╗  ██╔════╝╚██╗██╔╝██╔══██╗██╔══██╗██╔════╝██╔════╝██╔════╝
+╚█████╗░███████║██████╦╝██║░░██║██████╔╝  █████╗░░░╚███╔╝░██████╔╝██████╔╝█████╗░░╚█████╗░╚█████╗░
+░╚═══██╗██╔══██║██╔══██╗██║░░██║██╔══██╗  ██╔══╝░░░██╔██╗░██╔═══╝░██╔══██╗██╔══╝░░░╚═══██╗░╚═══██╗
+██████╔╝██║░░██║██████╦╝╚█████╔╝██║░░██║  ███████╗██╔╝╚██╗██║░░░░░██║░░██║███████╗██████╔╝██████╔╝
+╚═════╝░╚═╝░░╚═╝╚═════╝░░╚════╝░╚═╝░░╚═╝  ╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝░░╚═╝╚══════╝╚═════╝░╚═════╝░
+\n""")
+
+print('1. Cadastrar restaurante')
+print('2. Listar restaurante')
+print('3. Ativar restaurante')
+print('4. Sair\n')
+
+opcap_escolhida = input('Escolha uma opção: ')
+print(f'Você escolheu a opção {opcap_escolhida}.') #interpolação, usando o "f" antes das aspas
+
+
