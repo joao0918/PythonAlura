4bc219e794d2237cdab9aa1681695a99b45a9550
joao0918 <joaopedroaranda19@gmail.com>
2024-08-20 14:28:58 -0400

    update 20/08 sistema de shopcar pronto

--- ShopCar/ShopCar.py
+++ ShopCar/ShopCar.py
@@ -1,14 +1,132 @@
 from functions.class_carro import Carro
 from time import sleep
 from os import system
+
+carro1 = Carro('Ford', 2019, 30000, 20000)
+carro2 = Carro('Audi', 2021, 50000, 15000)
+carro3 = Carro('Ford', 2017, 25000, 30000)
+carro4 = Carro('BMW', 2018, 45000, 10000)
+
 def clear():
     system('cls')
+def logo_app():
+    print("""         ▄▄                                                         
+ ▄█▀▀▀█▄███                                ▄▄█▀▀▀█▄█                
+▄██    ▀███                              ▄██▀     ▀█                
+▀███▄    ███████▄   ▄██▀██▄▀████████▄    ██▀       ▀▄█▀██▄ ▀███▄███ 
+  ▀█████▄██    ██  ██▀   ▀██ ██   ▀██    ██        ██   ██   ██▀ ▀▀ 
+▄     ▀████    ██  ██     ██ ██    ██    ██▄        ▄█████   ██     
+██     ████    ██  ██▄   ▄██ ██   ▄██    ▀██▄     ▄▀█   ██   ██     
+█▀█████▀████  ████▄ ▀█████▀  ██████▀       ▀▀█████▀▀████▀██▄████▄   
+                             ██                                     
+                           ▄████▄                                   
+""")
+def close():
+    clear()
+    print('Obrigado pela confiança, volte logo...')
+    sleep(3)
+    exit()
+def exibir_erro(message):
+    clear()
+    print(message)
+    sleep(3)
+def exibir_title(mensage):
+    clear()
+    print(f'{mensage}\n')
+def lobby(time=3):
+    sleep(time)
+    input('\nAperte ENTER para voltar.')
+    exibir_title('Voltando para o menu.')
+    sleep(1)
+    main()
 
 
+def select_option():
+    logo_app()
+    print("""Slecione uma das opções a baixo: 
+    1 - Adcionar carro.
+    2 - Listar carros
+    3 - Listar Carros a venda
+    4 - Mudar Status do carro
+    5 - Sair""")
 
+    opcao = int(input('Opção selecionada: '))
+
+    try: 
+        if opcao == 1:
+            adcionar_carro()
+        elif opcao == 2:
+            mostrar_carros()
+        elif opcao == 3:
+            carros_a_venda()
+        elif opcao == 4:
+            mudar_status_do_carro()
+        elif opcao == 5:
+            close()
+        else:
+            exibir_erro('Opção Invalida, Tente novamente.')
+            main()
+
+    except ValueError:
+        exibir_erro('Opção Invalida, Tente novamente.')
+        main()
+
+def adcionar_carro():
+    exibir_title('Adicione um carro novo na loja')
+    
+    # Só saira do loop apos todos os dados forem inseridos
+    while True:
+        try:
+            marca_do_carro = input('Qual a Marca do carro: ').strip()
+            if not marca_do_carro:
+                raise ValueError("Marca do carro não pode estar em branco.")
+            
+            # Obter e verificar o ano do carro
+            ano_do_carro = input('Qual o ano do carro: ').strip()
+            if not ano_do_carro:
+                raise ValueError("O ano do carro não pode estar em branco.")
+            ano_do_carro = int(ano_do_carro)  # Convertendo após verificação
+            
+            # Obter e verificar o valor do carro
+            valor_do_carro = input('Qual o valor de venda: ').strip()
+            if not valor_do_carro:
+                raise ValueError("O valor de venda não pode estar em branco.")
+            valor_do_carro = int(valor_do_carro)  # Convertendo após verificação
+            
+            # Obter e verificar a quilometragem
+            km_rodado = input('Quantos quilômetros esse carro já rodou: ').strip()
+            if not km_rodado:
+                raise ValueError("A quilometragem não pode estar em branco.")
+            km_rodado = int(km_rodado)  # Convertendo após verificação
+
+            # Se todos os dados forem válidos, saímos do loop
+            break
+
+        except ValueError as e:
+            print(f"Erro: {e}. Por favor, insira os dados corretamente.")
+
+    new_car = Carro(marca_do_carro, ano_do_carro, valor_do_carro, km_rodado)
+    print(f"\nCarro {new_car._marca} adicionado com sucesso!")
+    lobby(1)
+
+def mostrar_carros():
+    exibir_title('Aqui está todos os carros')
+    Carro.lista_carros()
+    lobby(2)
+
+def carros_a_venda():
+    exibir_title('Carros a venda')
+    Carro.listar_carros_a_venda()
+    lobby(5)
+
+def mudar_status_do_carro():
+    exibir_title('Alterar Status do carro')
+    Carro.alterar_status_carro_por_marca()
+    lobby(2)
 
 def main():
-    clear
-
+    clear()
+    select_option()
+    
 if __name__ == '__main__':
     main()
--- ShopCar/functions/class_carro.py
+++ ShopCar/functions/class_carro.py
@@ -1,9 +1,10 @@
+from time import sleep
 class Carro:
     
     carros_adicionados = []
 
     def __init__(self, marca, ano, valor, km):
-        self._marca = marca.title()
+        self._marca = marca.upper()
         self._ano = ano
         self._valor = valor
         self._km = km
@@ -16,24 +17,70 @@ class Carro:
     
     @classmethod
     def lista_carros(cls):
-        print(f'{'Marca: '.ljust(15)} | {'Ano : '.ljust(15)} | {'Quilometragem: '.ljust(15)} | {'Valor: '.ljust(15)} | {'Status:'}')
+        print(f'{'Marca '.ljust(20)} | {'Ano '.ljust(20)} | {'Valor '.ljust(20)} | {'Quilometragem '.ljust(20)} | {'Status'}')
 
         carros_ordenados = sorted(cls.carros_adicionados, key=lambda carro: (carro._marca, carro._ano)) # essa função serve para ordenar a lista
 
         for carro in carros_ordenados:
-            (f'{carro._marca.ljust(15)} | {carro._ano.ljust(15)} | {carro._valor.ljust(15)} | {carro._km.ljust(15)} | {carro._status}')
+            print(f'{carro._marca.ljust(20)} | {str(carro._ano).ljust(20)} | {str(carro._valor).ljust(20)} | {str(carro._km).ljust(20)} | {carro._status}')
 
+    @classmethod
     def listar_carros_a_venda(cls):
-        print(f'{'Marca: '.ljust(15)} | {'Ano : '.ljust(15)} | {'Quilometragem: '.ljust(15)} | {'Valor: '.ljust(15)} | {'Status:'}')
+        print(f'{"Marca ".ljust(15)} | {"Ano ".ljust(15)} | {"Quilometragem ".ljust(15)} | {"Valor ".ljust(15)} | {"Status".ljust(15)}')
 
-        # aqui estamos filtrando oque desejamos, que seria os carros com status a venda e em negocioação
-        carros_a_venda = [carro for carro in cls.lista_carros if carro['status'] in ['A venda', 'Em Negociação']]
-                                # aqui esse acessa a lista e a ordena
-                                # Ordenando por marca (alfabética) e ano (do menor para o maior)
+        # Filtrando carros com status 'A venda' e 'Em Negociação'
+        carros_a_venda = [carro for carro in cls.carros_adicionados if carro._status in ['A venda', 'Em Negociação']]
+
+        # Ordenando por marca (alfabética) e ano (do menor para o maior)
         carros_ordenados = sorted(carros_a_venda, key=lambda carro: (carro._marca, carro._ano))
 
+        # Exibindo os carros
         for carro in carros_ordenados:
-            (f'{carro._marca.ljust(15)} | {carro._ano.ljust(15)} | {carro._valor.ljust(15)} | {carro._km.ljust(15)} | {carro._status}')
+            print(f'{carro._marca.ljust(15)} | {str(carro._ano).ljust(15)} | {str(carro._km).ljust(15)} | {str(carro._valor).ljust(15)} | {carro._status.ljust(15)}')
+
+    @classmethod
+    def alterar_status_carro_por_marca(cls):
+        tentativas = 3
+        while tentativas > 0:
+            marca_selesionada = input('Digite a marca do carro que deseja alterar: ')
+            carro_filtrado = [carro for carro in cls.carros_adicionados if carro._marca.lower() == marca_selesionada.lower()]
+            """
+            carros_filtrados = []
+            for carro in cls.carros_adicionados:
+                if carro._marca.lower() == marca_selecionada.lower():
+                carros_filtrados.append(carro)
+
+            # UTILIZANDO OPERADOR TERNARIO SIMPLIFICAMOS O CODIGO
+            """
+
+            if not marca_selesionada:
+                print("Nenhum carro encontrado para essa marca. Tente novamente.")
+                tentativas -= 1
+                sleep(3)
+                continue
+
+            print(f'\nCarros da marca {marca_selesionada.title()}:')
+            for idx, carro in enumerate(carro_filtrado):
+                print(f'{idx + 1} - {carro}')
+
+            try:
+                opcao = int(input(f'Selecione o número do carro que deseja alterar o estado (1-{len(carro_filtrado)}): '))
+                if 1 <= opcao <= len(carro_filtrado):
+                    carro_selecionado = carro_filtrado[opcao -1]
+                    carro_selecionado.alterar_status()
+                    print(f'O estado do carro "{carro_selecionado._marca} {carro_selecionado._ano}" foi alterado para: {carro_selecionado._status}')
+                    break
+                else:
+                    print("Opção inválida. Tente novamente.")
+            except ValueError:
+                print("Entrada inválida. Tente novamente.")
+
+            sleep(3)
+            tentativas -= 1
+        if tentativas == 0:
+            print('Não foi possível alterar o estado do carro. Voltando ao menu')
+        sleep(3)
+
 
     def alterar_status(self):
         print( """Selecione o Status do carro:
